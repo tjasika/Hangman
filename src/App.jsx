9138a566252f4de57055bb60dae5912eb7330e7c
Tjasa <tjasazzeleznik@gmail.com>
2025-10-27 21:13:12 +0100

    Initial commit + custom button test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello</h1>
+      <button className='btn-bg'>
+        A
+      </button>
+    </>
+  )
+}
+
+export default App
