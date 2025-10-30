83c97b7e6af244e9c177bab47030c99cde2c2bef
Tjasa <tjasazzeleznik@gmail.com>
2025-10-30 21:53:11 +0100

    Start page navigate

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -1,11 +1,14 @@
 import './StartPage.css'
+import { useNavigate } from 'react-router-dom'
 
 function StartPage() {
+   const navigate = useNavigate();
+
     return (
         <>
         <div className="start-container">
             <h1>HANGMAN</h1>
-            <button className='start-btn'>PLAY</button>
+            <button className='start-btn' onClick={()=>navigate('/game')}>PLAY</button>
         </div>
         </>
     )
