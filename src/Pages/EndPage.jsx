999cfd7da844b0e339691355a518393421441a49
Tjasa <tjasazzeleznik@gmail.com>
2025-10-30 22:27:40 +0100

    Game Over page

--- src/Pages/EndPage.jsx
+++ src/Pages/EndPage.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+import './StartPage.css'
+
 function EndPage() {
+    const navigate = useNavigate();
     return (
-        <div>Hello</div>
+        <>
+            <div className="start-container">
+                <h1>GAME OVER</h1>
+                <button className='start-btn' onClick={()=>navigate('/game')}>Play Again</button>
+            </div>
+        </>
+        
     )
 }
 
