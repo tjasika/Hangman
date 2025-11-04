1aa908408accd434db6043c9ebd85f32b1a60561
Tjasa <tjasazzeleznik@gmail.com>
2025-11-04 12:41:15 +0100

    Updates

--- src/Components/ImageContainer.jsx
+++
@@ -1,8 +0,0 @@
-import './ImageContainer.css'
-
-export const ImageContainer = ({imgUrl}) => {
-    return (
-        <div className='left-container' style={{backgroundImage: imgUrl}}>
-        </div>
-    )
-}
